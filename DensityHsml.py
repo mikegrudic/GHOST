correct smoothing length calculation

DensityHsml.py
@@ -7,8 +7,9 @@ hope.config.optimize=True
 def GetHsml(coords, des_ngb=32):
     tree = spatial.cKDTree(coords)
     neighbor_dists, neighbors = tree.query(coords, des_ngb)
+    if len(coords) < des_ngb:
+        return np.ones(len(coords))*neighbor_dists.max()
     hsml = np.empty(len(coords))
-#    t = time()
     GetHsmlWork(neighbors, neighbor_dists, hsml, des_ngb, len(hsml))
     return hsml
 
@@ -16,56 +17,27 @@ def GetHsml(coords, des_ngb=32):
 def GetHsmlWork(neighbors, neighbor_dists, hsml, des_ngb, N):
     n_ngb = 0.0
     for i in xrange(N):
-        upper = 1e-100
-        lower = 1e100
-        d=0.0
 
-        upper = neighbor_dists[i,des_ngb-1]*2
-        lower = neighbor_dists[i, 1]
+        upper = neighbor_dists[i,des_ngb-1]/0.63
+        lower = neighbor_dists[i, 2]
         error = 1e100
-
+        count = 0
         while error > 1:
-            midpoint = (upper+lower)/2
-            
-#            n_ngb = NNgb(neighbor_dists[i,:], midpoint, des_ngb)
+            h = (upper + lower)/2
             n_ngb=0.0
+            dngb=0.0
             q = 0.0
             for j in xrange(des_ngb):
-                q = neighbor_dists[i, j]/midpoint
+                q = neighbor_dists[i, j]/h
                 if q <= 0.5:
                     n_ngb += (1 - 6*q**2 + 6*q**3)
                 elif q <= 1.0:
-                    n_ngb += (2*(1-q)**3)
+                    n_ngb += 2*(1-q)**3
             n_ngb *= 32./3
             if n_ngb > des_ngb:
-                upper = midpoint
+                upper = h
             else:
-                lower = midpoint
-            error = np.fabs(n_ngb-des_ngb)
-        hsml[i] = midpoint
-
-#@hope.jit
-#def NNgb(dists, i, h, des_ngb):
-
-
-#    return n_ngb
-#    for i in xrange(num_pts):
-#        density = 0.0
-#        fval[:] = 0.0
-#        w = 0.0
-#        h = hsml[i]
-
-    
-#coords = np.load("/home/mgrudic/glass.npy")
-#r = np.sum(coords**2, axis=1)**0.5
-#coords = coordsB[r.argsort()][:1000]
-#r = r[r.argsort()][:1000]
-
-#r, coords = r/(r.max()), coords/(r.max())
-#coords = np.load("coords.npy")
-#r = np.sum(coords**2, axis=1)
-#hsml = GetHsml(np.ones(1e3), coords)
-#np.savetxt("hsml.txt", np.c_[hsml[r.argsort()],])
-#print (32.0 / 1000)**(1./3), hsml.mean(), np.sum(hsml**3)/32
-#np.savetxt("hsml.txt", np.c_[np.loadtxt("hsml.txt"), hsml])
-
+                lower = h
+            error = np.fabs(n_ngb-des_ngb)/des_ngb
+            
+        hsml[i] = h

GHOST.py
@@ -36,7 +36,7 @@ from PIL import Image, ImageDraw, ImageFont
 import h5py
 import numpy as np
 from yt.visualization import color_maps
-import option_d
+#import option_d
 from scipy import spatial
 from matplotlib.colors import LogNorm
 import re
@@ -62,13 +62,13 @@ imshow = arguments["--imshow"]
 
 font = ImageFont.truetype("LiberationSans-Regular.ttf", gridres/12)
 
-if n_ngb > 1:
+if nproc > 1:
     from joblib import Parallel, delayed, cpu_count
 
 G = 4.3e4
 
-nums = np.int_([fn.split('_')[1].split('.')[0] for fn in filenames])
-filenames = np.array(filenames)[np.argsort(nums)]
+#nums = np.int_([fn.split('_')[1].split('.')[0] for fn in filenames])
+#filenames = np.array(filenames)[np.argsort(nums)]
 
 @hope.jit
 def DepositDataToGrid(data, coords, N, hsml, gridres, rmax, griddata):
@@ -401,7 +401,7 @@ def Make2DPlots(data, plane='z', show_particles=False):
                 F.save(plotname)
                 F.close()
             else:
-                if zmin > 0:
+                if zmin > 0 and np.log10(np.abs(zmax)/np.abs(zmin)) > 2 and zmin != 0 and zmax != 0:
                     plot = ax.pcolormesh(X, Y, Z, norm=LogNorm(field_limits[field][0],field_limits[field][1]), antialiased=AA, cmap=colormap)
                 else:
                     plot = ax.pcolormesh(X, Y, Z, vmin=zmin, vmax=zmax, antialiased=AA, cmap="RdBu")